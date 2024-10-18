4159fb169096b4cec3feaed636a0fc8e00a06e46
sagebei <bei.zhou@imperial.ac.uk>
2024-10-18 15:34:18 +0100

    update

--- heat_equation_fem.py
+++ heat_equation_fem.py
@@ -6,7 +6,7 @@ from sovler import ConjugateGradient
 from utils import Visualization
 from matplotlib import tri
 from boundary import apply_dirichlet_boundary_conditions
-
+import time
 
 # Step 1: Define problem parameters
 L = 1000  # Length of domain in x and y directions
@@ -16,8 +16,8 @@ alpha = 6  # Thermal diffusivity
 # h = 0.5206164
 # print(h ** 2 / (2*alpha))
 dt = 0.0125  # Time step size
-nt = 3000  # Number of time steps
-ts_per_frame = 10 # record a frame every ts_per_frame time steps
+nt = 1000  # Number of time steps
+ts_per_frame = 10  # record a frame every ts_per_frame time steps
 
 device = torch.device("cuda" if torch.cuda.is_available() else "CPU")
 dtype = torch.float64
@@ -54,6 +54,7 @@ cg = ConjugateGradient(pcd)
 
 frames = u0.reshape((1, L, L))
 
+start = time.begin()
 print("solving")
 for n in range(1, nt):
     b = torch.sparse.mm(M_dt, u.unsqueeze(1)).squeeze(1)
@@ -69,6 +70,7 @@ for n in range(1, nt):
 
     if n % ts_per_frame == 0:
         frames = torch.cat((frames, u.reshape((1, L, L))))
+print(f"solved in: {time.time() - start} seconds")
 
 print("saving gif file")
 visualization = Visualization(frames, triangulation, dt, ts_per_frame)
