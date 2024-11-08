f30159e36d8e08229857a3064652c5200bf8d0b5
sagebei <bei.zhou@imperial.ac.uk>
2024-11-08 16:06:50 +0000

    update

--- solver.py
+++ solver.py
@@ -6,16 +6,21 @@ class ConjugateGradient:
     def __init__(self, preconditioner: Preconditioner):
         self.preconditioner = preconditioner
         self.x = None
+        self.x_prev = None
 
     def initialize(self, x): # Initial guess
         self.x = x.clone()
+        self.x_prev = x.clone()
 
     def solve(self, A, b, a_tol=1e-6, r_tol=1e-6, max_iter=100):
         total_iter = 0
 
+        x_clone = self.x.clone()
+        self.x.mul_(2).sub_(self.x_prev)
+        self.x_prev = x_clone
+
         r = b - A @ self.x # Initial residual
         z = self.preconditioner.apply(r)  # Preconditioned residual
-        
         p = z.clone()  # Initial search direction
 
         r_norm = torch.linalg.vector_norm(r)
