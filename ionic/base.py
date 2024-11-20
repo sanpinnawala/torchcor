586a7f3882fb4c2c2f9faa4bdefbfc9c9dcb8003
sagebei <bei.zhou@imperial.ac.uk>
2024-11-20 14:21:43 +0000

    update

--- ionic/base.py
+++ ionic/base.py
@@ -44,8 +44,8 @@ class BaseCellModel:
         rates = self.compute_rates(states=self.states, constants=self.constants)
         dU = rates[:, 0]
         
-        self.dt_ionic = self.dt / 10
-        for _ in range(10):
+        self.dt_ionic = self.dt / 20
+        for _ in range(20):
             dH = rates[:, 1:]
             self.H += self.dt_ionic * dH
             self.states[:, 1:] = self.H
