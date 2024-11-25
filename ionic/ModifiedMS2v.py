27613f01314640c588e8fd8a315c91ba3eaec17a
sagebei <bei.zhou@imperial.ac.uk>
2024-11-25 18:10:01 +0000

    update

--- ionic/ModifiedMS2v.py
+++ ionic/ModifiedMS2v.py
@@ -9,27 +9,42 @@ class ModifiedMS2v:
         self.tau_close = 120.0
         self.u_gate = 0.13
         self.u_crit = 0.13
+        self._vmin = -80.0
+        self._vmax = 20.0
+        self._DV  = self._vmax-self._vmin
+
 
         self.H = None
         self.dt = None
         self.device = device
         self.dtype = dtype
 
+
+    def to_dimensionless(self,U):
+        """ to_dimensionless(U) rescales U to its dimensionless values (range [0,1])
+        """
+        return(U-self._vmin)/self._DV
+
+    def derivative_to_dimensional(self,dU):
+        """ derivative_to_dimensional(U) rescales the derivative of U (dU) to dimensional values
+        """
+        return(self._DV*dU)
+
+
     def initialize(self, n_nodes, dt):
         self.H = torch.full(size=(n_nodes,), fill_value=1.0, device=self.device, dtype=self.dtype)
         self.dt = dt
 
-        u = torch.full(size=(n_nodes,), fill_value=0.0, device=self.device, dtype=self.dtype)
+        u = torch.full(size=(n_nodes,), fill_value=self._vmin, device=self.device, dtype=self.dtype)
         return u
 
     def differentiate(self, U):
-        J_in = -1.0 * self.H * U * (U - self.u_crit) * (1 - U) / self.tau_in
-        J_out = (1 - self.H) * U / self.tau_out
-        dU = - (J_in + J_out)
-
+        Uad   = self.to_dimensionless(U)
+        J_in  =  -1.0 * self.H * Uad * (Uad-self._u_crit) * (1.0-Uad)/self._tau_in
+        J_out =  (1.0-self.H)*Uad/self._tau_out
+        dU    = - self.derivative_to_dimensional(J_in +J_out)
         dH = torch.where(U > self.u_gate, -self.H / self.tau_close, (1 - self.H) / self.tau_open)
         self.H += self.dt * dH
-
         return dU
 
     def set_attribute(self, name, value):
@@ -45,9 +60,10 @@ class ModifiedMS2vRL(ModifiedMS2v):
 
     def differentiate(self, U):
         # Compute ionic currents
-        J_in = -1.0 * self.H * U * (U - self.u_crit) * (1 - U) / self.tau_in
-        J_out = (1 - self.H) * U / self.tau_out
-        dU = - (J_in + J_out)  # Membrane potential derivative
+        Uad   = self.to_dimensionless(U)
+        J_in  =  -1.0 * self.H * Uad * (Uad-self._u_crit) * (1.0-Uad)/self._tau_in
+        J_out =  (1.0-self.H)*Uad/self._tau_out
+        dU    = - self.derivative_to_dimensional(J_in +J_out)
 
         # Rush–Larsen update for H
         H_inf = torch.where(U > self.u_gate, 0.0, 1.0)  # Steady-state gating variable
