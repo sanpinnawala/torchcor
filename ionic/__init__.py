e183c8ca9629346eea03703529470470749233f7
sagebei <bei.zhou@imperial.ac.uk>
2024-11-27 14:38:43 +0000

    update

--- ionic/__init__.py
+++ ionic/__init__.py
@@ -1,7 +1,8 @@
-# from ionic.TenTusscherPanfilovLUT import TenTusscherPanfilov
 from ionic.ModifiedMS2v import ModifiedMS2v, ModifiedMS2vRL
-from ionic.CourtemancheRamirezNattel import CourtemancheRamirezNattel
 from ionic.MitchellSchaeffer import MitchellSchaeffer, MitchellSchaefferRL
+from ionic.CourtemancheRamirezNattel import CourtemancheRamirezNattel
+# from ionic.TenTusscherPanfilovLUT import TenTusscherPanfilov
+
 
 
 
