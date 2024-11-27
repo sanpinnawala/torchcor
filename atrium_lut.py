a7456b28ec6d04c0808af0f012f968f5513670ed
sagebei <bei.zhou@imperial.ac.uk>
2024-11-27 11:44:11 +0000

    update

--- atrium_lut.py
+++ atrium_lut.py
@@ -30,5 +30,5 @@ simulator.add_material_property(material_config)
 simulator.set_stimulus_region(path=f"{home_directory}/Data/atrium/Case_1.vtx")
 simulator.add_stimulus(stim_config)
 simulator.assemble()
-simulator.solve(a_tol=1e-5, r_tol=1e-5, max_iter=1000, plot_interval=simulation_time, verbose=True)
+simulator.solve(a_tol=1e-5, r_tol=1e-5, max_iter=1000, plot_interval=simulation_time, verbose=False)
 
--- monodomain/atrium_courtemanche.py
+++ monodomain/atrium_courtemanche.py
@@ -141,13 +141,13 @@ class AtriumSimulatorCourtemanche:
         for n in range(1, self.nt + 1):
             ctime += self.dt
             # if n > 80000:
-            #     print(f"-------------{n} timestep ----------------")
-            #     print("V max: ", u.max().item(), "V min: ", u.min().item())
+            print(f"-------------{n} timestep ----------------")
+            print("V max: ", u.max().item(), "V min: ", u.min().item())
 
             du = self.ionic_model.differentiate(u)
 
             # if n > 80000:
-            #     print("dV max: ", du.max().item(), "dV min: ", du.min().item())
+            print("dV max: ", du.max().item(), "dV min: ", du.min().item())
 
             b = u + self.dt * du
             for stimulus in self.stimuli:
