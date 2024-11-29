e8b61bcb76a987586701f46dded64d8ef5321e1c
sagebei <bei.zhou@imperial.ac.uk>
2024-11-29 13:52:29 +0000

    update

--- ventricle_lut.py
+++ ventricle_lut.py
@@ -3,7 +3,7 @@ from ionic import TenTusscherPanfilov
 import torch
 from pathlib import Path
 
-simulation_time = 50
+simulation_time = 100
 dt = 0.005
 stim_LV_sf = {'tstart': 0.0,
               'nstim': 1,
