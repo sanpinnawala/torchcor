dd1376ce16140608cdbed0a03102d4b4879e0c85
sagebei <bei.zhou@imperial.ac.uk>
2025-03-11 09:54:56 +0000

    update

--- benchmark/torchcor/surface.py
+++ benchmark/torchcor/surface.py
@@ -1,19 +1,21 @@
 import torchcor as tc
 from torchcor.simulator import Monodomain
-from torchcor.ionic import ModifiedMS2v
+from torchcor.ionic import ModifiedMS2v, CourtemancheRamirezNattel
 from pathlib import Path
 
 tc.set_device("cuda:0")
 simulation_time = 500
 dt = 0.01
 
-ionic_model = ModifiedMS2v(dt)
-ionic_model.u_gate = 0.1
-ionic_model.u_crit = 0.1
-ionic_model.tau_in = 0.15
-ionic_model.tau_out = 1.5
-ionic_model.tau_open = 105.0
-ionic_model.tau_close = 185.0
+# ionic_model = ModifiedMS2v(dt)
+# ionic_model.u_gate = 0.1
+# ionic_model.u_crit = 0.1
+# ionic_model.tau_in = 0.15
+# ionic_model.tau_out = 1.5
+# ionic_model.tau_open = 105.0
+# ionic_model.tau_close = 185.0
+
+ionic_model = CourtemancheRamirezNattel(dt)
 
 home_dir = Path(f"{Path.home()}/Data/surface")
 n_nodes_list = sorted([int(sub_dir.name) for sub_dir in home_dir.iterdir()])
