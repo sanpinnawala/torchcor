65fe30990267eb11191468d6f7861a552daaee12
sagebei <bei.zhou@imperial.ac.uk>
2025-03-12 11:18:01 +0000

    update

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # TorchCor
-TorchCor: Efficient Cardiac Electrophysiology
-Simulations on General Purpose GPUs
+TorchCor is a high-performance Python library for cardiac electrophysiology (CEP) simulations using the finite element method (FEM) on general-purpose GPUs. Built on top of PyTorch, TorchCor provides significant computational acceleration for CEP simulations, particularly with large mesh inputs.
+
+
+
 
 
 
--- demo/atrium.py
+++ demo/atrium.py
@@ -9,15 +9,15 @@ simulation_time = 500
 dt = 0.01
 
 
-# ionic_model = ModifiedMS2v(dt)
-# ionic_model.u_gate = 0.1
-# ionic_model.u_crit = 0.1
-# ionic_model.tau_in = 0.15
-# ionic_model.tau_out = 1.5
-# ionic_model.tau_open = 105.0
-# ionic_model.tau_close = 185.0
+ionic_model = ModifiedMS2v(dt)
+ionic_model.u_gate = 0.1
+ionic_model.u_crit = 0.1
+ionic_model.tau_in = 0.15
+ionic_model.tau_out = 1.5
+ionic_model.tau_open = 105.0
+ionic_model.tau_close = 185.0
 
-ionic_model = CourtemancheRamirezNattel(dt)
+# ionic_model = CourtemancheRamirezNattel(dt)
 
 home_dir = Path.home()
 case_name = f"Case_18"
@@ -39,4 +39,4 @@ simulator.solve(a_tol=1e-5,
                 linear_guess=True,
                 plot_interval=10, 
                 verbose=True,
-                format="")
+                format="vtk")
