202be01c2213587882c2aa14715e78d714db0cf0
sagebei <bei.zhou@imperial.ac.uk>
2025-04-17 16:40:40 +0100

    update

--- hpc/run.sh
+++ hpc/run.sh
@@ -5,7 +5,12 @@
 #$ -l h_vmem=11G
 #$ -l h_rt=240:0:0
 #$ -t 1-100
-#$ -l gpu=1         # request 1 GPU
+#$ -l gpu=1
+#$ -N conductivity
+
+module load miniforge
+module load cudnn/8.9.7.29-12-cuda-12.4.0-gcc-12.2.0
+conda activate venv
 
 
 python ./atrium.py -case_id ${SGE_TASK_ID}
