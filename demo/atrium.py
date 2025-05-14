f3fea4243618129f004d8aa7805fc37b990148c1
sagebei <bei.zhou@imperial.ac.uk>
2025-05-14 09:13:17 +0100

    update

--- demo/atrium.py
+++ demo/atrium.py
@@ -3,11 +3,15 @@ from torchcor.simulator import Monodomain
 from torchcor.ionic import ModifiedMS2v, CourtemancheRamirezNattel
 from pathlib import Path
 
+# Specify the GPU device to run the simulation on 
 tc.set_device("cuda:1")
 dtype = tc.float32
-simulation_time = 500
+# The total similation duration (ms)
+simulation_time = 600
+# time interval
 dt = 0.01
 
+# Load in the ionic model, such as MitchellSceaffer, CourtemancheRamirezNattel, or TenTussherPanfilov
 ionic_model = ModifiedMS2v(dt, dtype=dtype)
 ionic_model.u_gate = 0.1
 ionic_model.u_crit = 0.1
@@ -16,29 +20,30 @@ ionic_model.tau_out = 1.5
 ionic_model.tau_open = 105.0
 ionic_model.tau_close = 185.0
 
-# ionic_model = CourtemancheRamirezNattel(dt)
-
 case_name = "Case_10"
 mesh_dir = Path("/home/bzhou6/Data/atrium/") / case_name
 
+# 1. Initialise the Mondomain model
 simulator = Monodomain(ionic_model, T=simulation_time, dt=dt, dtype=dtype)
+# 2. Load in the mesh files (.pts .elem .lon)
 simulator.load_mesh(path=mesh_dir, unit_conversion=1000)
-simulator.add_condutivity(region_ids=[1, 2, 3, 4, 5, 6], il=0.3, it=0.06, el=0.4, et=0.4)
-
+# 3. Specify the conductivity for each region
+simulator.add_condutivity(region_ids=[1, 2, 3, 4, 5, 6], il=0.3, it=0.06, el=None, et=None)
+# 4. Specify the locations where stimulation is applied
 simulator.add_stimulus(f"{mesh_dir}/{case_name}.vtx", 
                        start=0.0, 
                        duration=2.0, 
                        intensity=50)
-
-simulator.solve(a_tol=1e-5, 
-                r_tol=1e-5, 
-                max_iter=100, 
-                calculate_AT_RT=True,
+# 5. Start the simulation
+simulator.solve(a_tol=1e-5,              # absolute tolerance
+                r_tol=1e-5,              # relative tolerance
+                max_iter=100,            # maximum number of iterations for each CG calculation
+                calculate_AT_RT=True,    # keep track of local activation time (LAT)
                 linear_guess=True,
-                snapshot_interval=1, 
+                snapshot_interval=1,     # save the soluation after every 10 ms
                 verbose=True,
-                result_path="./atrium")
+                result_path="./atrium")  # to folder in which the results are saved
 
-# simulator.pt_to_vtk()
-simulator.phie_recovery()
-simulator.simulated_ECG()
+simulator.pt_to_vtk()  # generate VTK files 
+# simulator.phie_recovery()
+# simulator.simulated_ECG()
